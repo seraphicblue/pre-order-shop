153ba1b486e3177d5ca48895e68637f9e8ed950a
seraphicblue <ggj05037@gmail.com>
2024-02-22 14:36:40 +0900

    dev 브랜치에 변경사항 적용

---
+++ src/main/java/com/example/preordershop/PreordershopApplication.java
@@ -0,0 +1,13 @@
+package com.example.preordershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreordershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreordershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/preordershop/PreordershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preordershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreordershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
